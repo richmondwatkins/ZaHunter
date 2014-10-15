3a8ee4db8730dc4c9902a9533e0df820bc4817e9
Richmond Watkins <richmondwatkins@mac.com>
2014-10-15 14:31:16 -0500

    Initial Commit

---
+++ ZaHunter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZaHunter
+//
+//  Created by Richmond on 10/15/14.
+//  Copyright (c) 2014 Richmond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZaHunter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZaHunter
+//
+//  Created by Richmond on 10/15/14.
+//  Copyright (c) 2014 Richmond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
